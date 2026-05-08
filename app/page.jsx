0f5f971e1b2bc7a0c126729c5ad802f6ce01082a
wasabu <iamalexandervorobiev@gmail.com>
2026-05-08 13:02:34 +0700

    page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,8 @@
+"use client";
+
 import nextDynamic from "next/dynamic";
 
 const Game = nextDynamic(() => import("./Game"), { ssr: false });
-
-// Disable static prerendering - page is fully client-side
-export const dynamic = "force-dynamic";
 
 export default function Page() {
   return <Game />;
